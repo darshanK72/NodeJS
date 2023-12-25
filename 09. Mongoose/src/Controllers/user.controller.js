310743eab19db3cc23a07ab7f80cfa284f9ff4a6
darshan72 <darshankhairnar72@gmail.com>
2023-12-25 12:07:19 +0530

    completed crud operations using mongoose

--- 09. Mongoose/src/Controllers/user.controller.js
+++ 09. Mongoose/src/Controllers/user.controller.js
@@ -1,17 +1,18 @@
 import { User } from "../Models/user.model.js";
 
 export const getUsers = async (req, res) => {
-    try{
-        const users = await User.find();
+    const query = req.query;
+    try {
+        const users = await User.find(query);
         res.status(200).json(users);
-    }catch(error){
+    } catch (error) {
         res.status(500).send("Internal Server Error");
     }
 }
 
 export const getUserById = async (req, res) => {
     const id = req.params.id;
-    try{
+    try {
         const user = await User.findById(id);
         if (user) {
             res.status(200).json(user);
@@ -19,7 +20,7 @@ export const getUserById = async (req, res) => {
             res.status(404).json({ message: "User Not Found" });
         }
     }
-    catch(error){
+    catch (error) {
         res.status(500).send("Internal Server Error");
     }
 }
@@ -30,6 +31,34 @@ export const addUser = async (req, res) => {
         res.status(201).json({ message: "User is created", user });
     } catch (error) {
         res.status(400).json({ message: "Invalid Input", error });
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, upsert: true });
+        if (user) {
+            res.status(201).json(user);
+        } else {
+            res.status(404).json({ message: "User Not Found" });
+        }
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "User Not Found" });
+        }
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
     }
 }
 
--- 09. Mongoose/src/Routes/user.route.js
+++ 09. Mongoose/src/Routes/user.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { addUser, getUsers,getUserById } from "../Controllers/user.controller.js";
+import { addUser, getUsers,getUserById, updateUser , deleteUser} from "../Controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/").get(getUsers).post(addUser);
-router.route("/:id").get(getUserById);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 export default router;
